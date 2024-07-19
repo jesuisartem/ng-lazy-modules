8483a2fa3787821752b7ecb5db5b0f8e4a949de1
Artem <jesuisartem@gmail.com>
2024-07-20 01:19:56 +0500

    add menu item selection after reload

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,12 @@
+import {Component, inject} from '@angular/core';
+import {AuthService} from "../services/auth.service";
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent {
+  public authService = inject(AuthService);
+
+}
