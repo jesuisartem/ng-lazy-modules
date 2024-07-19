574adfff3977e6d019f00712dec02bc49a713630
Artem <jesuisartem@gmail.com>
2024-07-19 19:18:55 +0500

    init project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MenuItem } from "primeng/api";
+import {BehaviorSubject} from "rxjs";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'testmultimodule';
+  public menuItems: MenuItem[] = [];
+
+  public isSidebarVisible$ = new BehaviorSubject<boolean>(false);
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {MenubarModule} from "primeng/menubar";
+import {Button} from "primeng/button";
+import {SidebarModule} from "primeng/sidebar";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -10,7 +14,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MenubarModule,
+    Button,
+    SidebarModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
