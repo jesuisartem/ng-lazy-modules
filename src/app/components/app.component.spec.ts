be133ab7be09c17dc81aacbcf8ae3ce31610b336
jesuisartem <jesuisartem@gmail.com>
2024-07-20 17:15:14 +0500

    rename project

--- src/app/components/app.component.spec.ts
+++ src/app/components/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'testmultimodule'`, () => {
+  it(`should have as title 'ng-lazy-modules'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('testmultimodule');
+    expect(app.title).toEqual('ng-lazy-modules');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('testmultimodule app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('ng-lazy-modules app is running!');
   });
 });
